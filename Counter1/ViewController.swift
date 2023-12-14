237347736dae228f3272bc9204b40ec03c67d0d5
KostyanTs <strong.coy@gmail.com>
2023-12-14 14:55:14 +0600

    Get a button that change counter each counter

--- Counter1/ViewController.swift
+++ Counter1/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var changeCountButton: UIButton!
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    var counter: Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        changeCountButton.backgroundColor = .gray
+        changeCountButton.frame.size.width = 200
+        changeCountButton.frame.size.height = 200
+        changeCountButton.layer.cornerRadius = 100
+        changeCountButton.layer.masksToBounds = false
     }
 
-
+    @IBAction func buttonTouchDown(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "\(counter)"
+    }
+    
 }
 

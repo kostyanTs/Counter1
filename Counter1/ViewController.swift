6726c2d075b19a1cf64bb795860f8bc7da2c4d18
KostyanTs <strong.coy@gmail.com>
2023-12-14 15:48:37 +0600

    Create plus and minus buttons for change counerLabel Create removeBotton for change counterLabel to 0

--- Counter1/ViewController.swift
+++ Counter1/ViewController.swift
@@ -9,27 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var changeCountButton: UIButton!
-    
     @IBOutlet weak var counterLabel: UILabel!
     
-    var counter: Int = 0
+    @IBOutlet weak var plusCounterButton: UIButton!
     
+    @IBOutlet weak var minusCounterButton: UIButton!
+    
+    @IBOutlet weak var zeroButton: UIButton!
+    var counter: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        changeCountButton.backgroundColor = .gray
-        changeCountButton.frame.size.width = 200
-        changeCountButton.frame.size.height = 200
-        changeCountButton.layer.cornerRadius = 100
-        changeCountButton.layer.masksToBounds = false
+        plusCounterButton.tintColor = .red
     }
-
-    @IBAction func buttonTouchDown(_ sender: Any) {
+    
+    @IBAction func plusTouchUp(_ sender: Any) {
         counter += 1
         counterLabel.text = "\(counter)"
     }
     
+    @IBAction func minusTouchUp(_ sender: Any) {
+        counter -= 1
+        if counter < 0 {
+            counter = 0
+        }
+        counterLabel.text = "\(counter)"
+    }
+    @IBAction func zeroTouchUp(_ sender: Any) {
+        counter = 0
+        counterLabel.text = "\(counter)"
+    }
 }
 
